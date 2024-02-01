ec70add2bb97c657c72991441b0a74b094c0e465
unknown <rilesconnors@gmail.com>
2024-02-01 15:46:09 -0600

    hey

---
+++ Utils/utils.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "bnet_interface.h"
+#include "utils.h"
+
+void monitorStatus(bnet_interface bnet) {
+
+}
---
+++ Utils/utils.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include "bnet_interface.h"
+
+void monitorStatus(bnet_interface bnet) {};
