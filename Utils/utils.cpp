4420a12dbcdbf1991a6680d18038496fe66e89c8
Riley Connors <rpczdb@umsystem.edu>
2024-02-05 16:43:46 -0600

    test

--- Utils/utils.cpp
+++ Utils/utils.cpp
@@ -3,12 +3,14 @@
 #include "utils.h"
 #include "thread"
 #include "future"
+#include "atomic"
 
 
 void monitorStatus( bnet_interface& bnet) {
-	
-	for (int i = 0; i < 10; i++) {
+	std::atomic<bool> running(true);
+	bnet.set_collect(STATUS_DATA, true);
+	while (running) {
 		std::this_thread::sleep_for(std::chrono::seconds(5)); 
-		std::cout << "System state: " << bnet.get_status().data->eth_link_rate << endl;
+		std::cout << "System state: " << bnet.get_status().data->sys_state << endl;
 	}
 }
