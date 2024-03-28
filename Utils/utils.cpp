fcc583c7db0dfa50f5de934ef8fe34b5f29ac5de
Riley Connors <rpczdb@umsystem.edu>
2024-03-28 10:45:46 -0500

    please work

--- Utils/utils.cpp
+++ Utils/utils.cpp
@@ -4,24 +4,60 @@
 #include "thread"
 #include "future"
 #include "atomic"
+#include <ctime>
+#include <utility>
+#include <string>
+#include <sstream>
 
-
-
+void send_command(bnet_interface& bnet, std::string command)
+{
+	std::string result = bnet.send_command(command).second;
+	std::cout << result << std::endl;
+}
 
 void startupScript(bnet_interface& bnet) {
 	std::string command, output;
 	command = "RESET:PARAMETERS";
 	bnet.send_command(command);
+
+
+
+	command = "SYS:TIME 0,0";
+	bnet.send_command(command);
+
+	auto now = std::chrono::system_clock::now();
+	auto unixTime = std::chrono::system_clock::to_time_t(now);
+	auto days = unixTime / 86400;
+	long long milliseconds = (unixTime % 86400) * 1000LL;
+
+	std::pair<int, int> bootTime = { days, milliseconds };
+	std::string token;
+	std::istringstream tokenStream(output);
+	command = "SYS:TIME?";
+    output = bnet.send_command(command).second;
+	while (std::getline(tokenStream, token, ',')) {
+		bootTime.first = std::stoi(token);
+		std::getline(tokenStream, token, ',');
+		bootTime.second = std::stoi(token);
+	}
+	milliseconds -= bootTime.second;
+	days -= bootTime.first;
+	if (milliseconds < 0) {
+		milliseconds += 86400000;
+		days--;
+	}
+	command = "SYS:TIME " + std::to_string(days) + "," + std::to_string(milliseconds);
+
 	command = "MODE:SWT:OPERATIONMODE 1";
 	bnet.send_command(command);
-	command = "enable_track";
-	bnet.send_command(command);
+	bnet.set_logging(true);
+	bnet.set_collect(TRACK_DATA, true);
+	bnet.set_save(TRACK_DATA, true);
 	command = "enable_track_logging";
 	bnet.send_command(command);
-	bnet.set_collect(TRACK_DATA, true);
-	bnet.set_logging(TRACK_DATA);
-	bnet.set_save(TRACK_DATA, true);
-	command = "MODE:SWT:TRACK:ELFOVMIN -20";
+	command = "MODE:SWT:TRACK:ELFOVMIN -10";
+	bnet.send_command(command);
+	command = "MODE:SWT:TRACK:AZFOVMAX 10";
 	bnet.send_command(command);
 	command = "RSP:RCSMASK:MAXRCS 10";
 	bnet.send_command(command);
@@ -48,37 +84,33 @@ int createSocket(int& sock, struct sockaddr_in& serv_addr) {
 		return -1;
 	}
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(1111);
+	serv_addr.sin_port = htons(8080);
 
 	if (inet_pton(AF_INET, "192.168.1.11", &serv_addr.sin_addr) < 0) {
 		std::cout << "Invalid address" << std::endl;
 		return -1;
 	}
-
 	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
 		std::cout << "Connection failed" << std::endl;
 		return -1;
 	}
-
 	std::cout << "Socket created" << std::endl;
-
 	return 1;
 }
+
 
 coordinateStruct getCoordinates(bnet_interface& bnet) {
 	coordinateStruct coords{};
 	if (bnet.get_track().header->nTracks > 0) {
 		coords.az = bnet.get_track().data.at(0).azest;
 		coords.el = bnet.get_track().data.at(0).elest;
-		coords.range = bnet.get_track().data.at(0).rest;
 	}
 	else {}
 	return coords;
 }
 
-unsigned char* serializeCoordinates(coordinateStruct coords)
+void serializeCoordinates(coordinateStruct coords, unsigned char* buffer)
 {
-	unsigned char buffer[sizeof(float) * 3];
 	unsigned char* azimuthBytes = reinterpret_cast<unsigned char*>(&coords.az);
 	for (size_t i = 0; i < sizeof(float); ++i) {
 		buffer[i] = azimuthBytes[sizeof(float) - 1 - i];
@@ -87,30 +119,26 @@ unsigned char* serializeCoordinates(coordinateStruct coords)
 	for (size_t i = 0; i < sizeof(float); ++i) {
 		buffer[sizeof(float) + i] = elevationBytes[sizeof(float) - 1 - i];
 	}
-	unsigned char* rangeBytes = reinterpret_cast<unsigned char*>(&coords.range);
-	for (size_t i = 0; i < sizeof(float); ++i) {
-		buffer[2 * sizeof(float) + i] = rangeBytes[sizeof(float) - 1 - i];
-	}
-	return buffer;
 }
 
 //get coordinates of most likely UAV. Assumes there is at least one active track
 coordinateStruct getMostUAV(bnet_interface& bnet)
 {
-	float az, el, range = 0;
+	float az, el = 0;
 	float pUAV = 0;
 	int target = 0;
-	for (int i = 0; i < bnet.get_track().header->nTracks; i++) {
-		if (bnet.get_track().data.at(i).probabilityUAV > pUAV) {
-			pUAV = bnet.get_track().data.at(i).probabilityUAV;
-			target = i;
+	if (bnet.get_track().header->nTracks > 1) {
+		for (int i = 0; i < bnet.get_track().header->nTracks; i++) {
+			if (bnet.get_track().data.at(i).probabilityUAV > pUAV) {
+				pUAV = bnet.get_track().data.at(i).probabilityUAV;
+				target = i;
+			}
 		}
 	}
 	az = bnet.get_track().data.at(target).azest;
 	el = bnet.get_track().data.at(target).elest;
-	range = bnet.get_track().data.at(target).rest;
 	
-	return coordinateStruct(az, el, range);
+	return coordinateStruct(az, el);
 }
 
 
