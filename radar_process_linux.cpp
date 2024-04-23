80dc89b362fceb3add42c38d4cea35a1cd428200
Riley Connors <rpczdb@umsystem.edu>
2024-04-23 14:28:19 -0500

    almost done with refactoring

--- radar_process_linux.cpp
+++ radar_process_linux.cpp
@@ -2,9 +2,9 @@
 //
 
 #include <iostream>
-#include "bnet_async.h"
+#include "bnet_interface.h"
 #include <string> 
-#include <pthread.h>
+#include <thread>
 #include <vector>
 #include <fstream>
 #include "utils.h"
@@ -22,7 +22,6 @@ int piSock, processSock = 0;
 struct sockaddr_in pi_serv_add, process_serv_add;
 sig_atomic_t exitLoop = 0;
 std::string command;
-unsigned char trackBuffer[8] = { 0 };
 int heading;
 
 //this will be used to let CTRL+C stop the main tracking loop without exiting the program
@@ -42,11 +41,11 @@ int main()
 	}
 	//create the socket to the raspberry pi (rpi), create a bnet object and connect to radar, and issue initial startup script
 	int socketCreated = createPiSocket(piSock, pi_serv_add);
-    int processSocketCreated = createProcessSocket(processSock, process_serv_add);
 	std::cout << "Let's track some bad guys\n" << std::endl;
 	bnet_interface bnet_commands;
 	bnet_commands.connect(ip, port, custom_directory, 60000L);
 	startupScript(bnet_commands);
+	std::thread mainThread(mainLoop, std::ref(piSock), socketCreated, std::ref(processSock), std::ref(process_serv_add), std::ref(exitLoop));
 
 	// loop to enter any preliminary commands before tracking. C or c will start to the main tracking loop
 	while (true) {
@@ -62,92 +61,13 @@ int main()
 		send_command(bnet_commands, command);
 	}
 
-	//set up the csv file for logging
-	std::ofstream outfile;
-	std::string filename;
-	std::cout << "Enter test name: " << std::endl;
-	std::cin >> filename;
-	filename += getTimeString();
-	outfile.open(filename);
-	outfile << "time,rvx,rvy,rvz,raz,rel,rrange,kvx,kvy,kvz,kaz,kel" << std::endl;
-
 	//start tracking and give it a second to pick up a track
 	send_command(bnet_commands, "MODE:SWT:START");
-	
-	/*Set preliminary stuff: set signal ending the tracking loop, create kalman filter object,
-	and some other random variables it'll use*/
-	signal(SIGINT, sig_handler);
-	std::cout << "Press CTRL + C to stop tracking" << std::endl;
-	KalmanFilter* kf = new KalmanFilter;
-	Eigen::VectorXd x0(7);
-	Eigen::VectorXd z(5);
-	double lastTime;
-	coordinateStruct toTrack;
-	int trackID = -1;
-	int bufferedTracks = 0;
-    uint8_t trackData[2600];
+
 
 	/*main tracking loop. CTRL+C will exit this when done. Sends radar data to rpi, and also logs both radar data
-	and Kalman filtered data*/
-	while (!exitLoop) {
-        int bytesReceived = recv(processSock, trackData, 2600, 0);
-        if (bytesReceived < 0) {
-            std::cout << "Error receiving data from process" << std::endl;
-            continue;
-        }
-        else if (bytesReceived == 0) {
-            std::cout << "Process disconnected" << std::endl;
-            break;
-        }
-        else {
-            parsed_packet packet = parseTrackPacket(trackData, bytesReceived);
-            bufferedTracks = packet.header.nTracks;
-            //if there's a packet in the buffer, do this:
-            if (bufferedTracks > 0) {
-                toTrack = getMostUAV(packet);
-
-                /*if the packet has at least one track, and the most probable track in the packet is not the current track, re-initialize the kalman filter
-                and give it a 50 millisecond update*/
-                if (toTrack.id != trackID) {
-                    trackID = toTrack.id;
-                    lastTime = toTrack.lastTime;
-                    std::cout << "Now tracking UAV " << trackID << std::endl;
-                    x0 << toTrack.vx, toTrack.vy, toTrack.vz, toTrack.az, toTrack.el, 0, 0;
-                    z << toTrack.vx, toTrack.vy, toTrack.vz, toTrack.az, toTrack.el;
-                    kf->init(x0, 1, trackID);
-                    kf->predict(.05);
-                    kf->update(z);
-                    std::cout << "kalman updated" << std::endl;
-                }
-
-                /*if the packet grabbed is tracking the same object as before, update the kalman filter based on difference between last recorded track time
-                and the previously last recorded track time*/
-                else if (toTrack.id == trackID) {
-                    std::cout << "still tracking " << trackID << std::endl;
-                    z << toTrack.vx, toTrack.vy, toTrack.vz, toTrack.az, toTrack.el;
-                    double dt = (toTrack.lastTime - lastTime) / 1000;
-                    kf->predict(dt);
-                    kf->update(z);
-                }
-
-                if (socketCreated == 1) {
-                    serializeCoordinates(toTrack, trackBuffer);
-                    std::cout << send(piSock, trackBuffer, sizeof(float) * 2, 0) << " bytes sent to gimbal"
-                              << std::endl;
-                }
-
-                //print radar and kalman coordinates to csv
-                outfile << toTrack.lastTime << "," << toTrack.vx << "," << toTrack.vy << "," << toTrack.vz << ","
-                        << toTrack.az << "," << toTrack.el << "," << toTrack.range << ",";
-                outfile << kf->get_x_hat()[0] << "," << kf->get_x_hat()[1] << "," << kf->get_x_hat()[2] << ","
-                        << kf->get_x_hat()[3] << "," << kf->get_x_hat()[4] << std::endl;
-            }
-                //if there's no packet in the buffer, wait a moment and check for another packet
-            else {
-                continue;
-            }
-        }
-	}
+    and Kalman filtered data*/
+	mainThread.join();
 	
 	//stop tracking, stop logging, and disconnect from the radar
 	send_command(bnet_commands, "MODE:SWT:STOP");
@@ -160,6 +80,7 @@ int main()
 
 	//close the socket with the rpi
 	close(piSock);
+	close(processSock);
 	std::cout << "Socket closed" << std::endl;
 	return 0;
 }
