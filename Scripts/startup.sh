b55334fa0af115edc62c7cfcae7d236f0d1bf5ec
Riley Connors <rpczdb@umsystem.edu>
2024-04-29 16:16:15 -0500

    update startup script

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-wsl.exe /bin/bash -c "
-    sudo python /mnt/c/Users/CUAV/Documents/Projects/radar_process_linux/Scripts/proxy.py & <<EOF
+echo "Opening python proxy server..."
+wsl.exe bash -c "
+sudo python /mnt/c/Users/CUAV/Documents/Projects/radar_process_linux/Scripts/proxy.py <<EOF
 cuav
-EOF
-"
+EOF;
+exec bash
+" &
 
-wsl
-wsl.exe /bin/bash -c "
-    ssh radarpi@192.168.1.11 << ENDSSH
-    source gimbal_control_env/bin/activate
-    cd gimbal
-    sudo python gimbal_server.py
-ENDSSH
-"
+echo "Proxy server running\nStarting radar processor..."
+
+wsl.exe bash -c "
+radar_process & <<EOF
+u
+EOF
+exec bash
+" &
+
+echo "Processor running\nConnecting to Pi..."
+
+wsl.exe bash -c "
+    ssh radarpi@192.168.1.11 << EOF
+source gimbal_control_env/bin/activate
+cd gimbal
+sudo python gimbal_server.py
+exec bash
+EOF
+" &
+
+echo "Connected to Pi. Opening RadarUI..."
+
+"C:\Users\CUAV\Documents\Projects\EchoDyne Radar\Radar-UI-v1.3.0\radarui.bat"
