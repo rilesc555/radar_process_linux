02dc2aa1e17b610c7f55666037e13aa909c92ff0
Riley Connors <rpczdb@umsystem.edu>
2024-04-29 16:38:34 -0500

    script still messed up

--- Scripts/startup.bat
+++ Scripts/startup.bat
@@ -2,18 +2,25 @@
 
 echo Opening python proxy server...
 
-start "" wsl.exe bash -c "sudo python /mnt/c/Users/CUAV/Documents/Projects/radar_process_linux/Scripts/proxy.py" < nul
+start "" wsl.exe bash -c "sudo python /mnt/c/Users/CUAV/Documents/Projects/radar_process_linux/Scripts/proxy.py; cuav; exec bash" < nul
 
-echo Proxy server running\nStarting radar processor...
+echo Proxy server running
+echo Starting radar processor...
 
-start "" wsl.exe bash -c "radar_process" < nul
+start "" wsl.exe bash -c "cd"
 
-echo Processor running\nConnecting to Pi...
+ping -n 1 127.0.0.1 > nul
 
-start "" wsl.exe bash -c "ssh radarpi@192.168.1.11 << EOF
+wsl.exe bash -c "radar_process; u; exec bash" < nul
+
+echo Processor running
+echo Connecting to Pi...
+
+start /wait "" wsl.exe bash -c "ssh radarpi@192.168.1.11 << EOF
 source gimbal_control_env/bin/activate
 cd gimbal
 sudo python gimbal_server.py
+cuav
 EOF
 "
 
