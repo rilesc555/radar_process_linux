7b013ea4439dbe7b337d86bb3428897bbd137cbb
Riley Connors <rpczdb@umsystem.edu>
2024-05-01 16:51:12 -0500

    damn opening wsl really tough

--- Scripts/startup.py
+++ Scripts/startup.py
@@ -9,19 +9,23 @@ def execute_command(command):
 
 # Task 1: Open a WSL window, start a Python script in sudo mode, and input the admin password
 def startProxy():
-    print("Task 1: Running Python script in WSL with sudo")
-    command = 'wsl echo "password" | sudo -S python3 script.py'
-    output, error = execute_command(command)
-    print("Output:", output)
-    print("Error:", error)
+    
+    commands = "cd; echo 'cuav' | sudo -S python /mnt/c/Users/CUAV/Documents/Projects/radar_process_linux/Scripts/proxy.py"
+    subprocess.Popen(['wsl', 'bash', '-c', commands], creationflags=subprocess.CREATE_NEW_CONSOLE)
+
 
 # Task 2: Open a new WSL window, start a program, input commands, and keep it running
 def startProcessor():
-    print("Task 2: Starting a program in WSL and keeping it running")
-    command = 'wsl bash -c "echo commands | program"'
-    process = subprocess.Popen(command, shell=True)
-    time.sleep(5)  # Wait for the program to start
-    print("Program is running in the background")
+    commands = '''cd 
+    /home/cuav/.vs/radar_process_linux/out/build/linux-release/radar_process
+    exec bash'''
+    process = subprocess.Popen(['wsl', 'bash', '-c', commands], creationflags=subprocess.CREATE_NEW_CONSOLE, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,)
+    commands = '''u'''
+    out, err = process.communicate(input=commands)
+    print(out)
+    if err:
+        print(err)
+  
 
 # Task 3: Open a new WSL window, SSH to a Raspberry Pi, wait for the connection, input commands, and start a Python script
 def startPiServer():
@@ -42,11 +46,9 @@ def startUI():
 # Main function to run the tasks
 def main():
     # startProxy()
-    # startProcessor()
+    startProcessor()
     # startPiServer()
     # startUI()
-
-    subprocess.run(["wsl", "sudo python /mnt/c/Users/CUAV/Documents/Projects/radar_process_linux/Scripts/proxy.py", "cuav", "exec bash"], shell=True)
 
 if __name__ == '__main__':
     main()
